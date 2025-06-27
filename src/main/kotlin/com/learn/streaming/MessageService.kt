6b228fea642900f05bb0b138b93263c1d83a1050
Elias Rahmani <eliass.rahmani@gmail.com>
2025-06-27 22:41:04 +0200

    add gen ai
    Signed-off-by: Elias Rahmani <eliass.rahmani@gmail.com>

--- src/main/kotlin/com/learn/streaming/MessageService.kt
+++ src/main/kotlin/com/learn/streaming/MessageService.kt
@@ -1,15 +1,46 @@
 package com.learn.streaming
 
-import org.springframework.ai.document.Document
+import org.springframework.ai.chat.prompt.PromptTemplate
 import org.springframework.ai.vectorstore.SearchRequest
 import org.springframework.ai.vectorstore.VectorStore
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Service
 
+
 @Service
-class MessageService(val vectorStore: VectorStore) {
-    fun getMessages(prompt: String, limit: Int): List<Document?>? {
-        return vectorStore.similaritySearch(
-            SearchRequest.builder().query(prompt).topK(limit).build()
+class MessageService(val vectorStore: VectorStore, val jdbcTemplate: JdbcTemplate) {
+    private val promptText = """
+                Answer the following question based on the provided context.
+                If the answer is not found in the context, please state that you don't have enough information.
+
+                Context:
+                {context}
+
+                Question:
+                {query}
+                """
+
+    fun getMessages(prompt: String, limit: Int): String? {
+        val similarDocuments = vectorStore.similaritySearch(
+            SearchRequest.builder()
+                .query(prompt).topK(limit)
+                .build()
+        )
+        if (similarDocuments == null) return null
+        val context: String? = similarDocuments
+            .joinToString("\n\n") { it.text.toString() }
+        val promptTemplate = PromptTemplate(promptText)
+        val render = promptTemplate.render(
+            mapOf(
+                "context" to context,
+                "query" to prompt
+            )
+        )
+        val sql = "SELECT pgml.transform('summarization', ?)"
+        return jdbcTemplate.queryForObject(
+            sql,
+            String::class.java,
+            render
         )
     }
 }
