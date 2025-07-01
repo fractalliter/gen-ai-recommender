77963456966b3e1e61a73438eb5c6d0c5573bddb
Elias Rahmani <eliass.rahmani@gmail.com>
2025-07-01 16:46:59 +0200

    create json out of result

--- src/main/kotlin/com/learn/streaming/MessageService.kt
+++ src/main/kotlin/com/learn/streaming/MessageService.kt
@@ -1,32 +1,43 @@
 package com.learn.streaming
 
-import org.springframework.ai.chat.prompt.PromptTemplate
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.ai.document.Document
 import org.springframework.ai.vectorstore.SearchRequest
 import org.springframework.ai.vectorstore.VectorStore
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Service
 
-
 @Service
-class MessageService(val vectorStore: VectorStore, val jdbcTemplate: JdbcTemplate) {
+class MessageService(
+    private val vectorStore: VectorStore, 
+    private val jdbcTemplate: JdbcTemplate,
+    private val objectMapper: ObjectMapper
+) {
 
-    fun getMessages(prompt: String, limit: Int): String? {
-         val searchRequest = SearchRequest.builder()
-            .query(prompt).topK(limit)
-            .build()
+    fun getMessages(prompt: String, limit: Int): Result? {
+        val searchRequest = SearchRequest.builder().query(prompt).topK(limit).build()
         val similarDocuments: List<Document>? = vectorStore.similaritySearch(searchRequest)
         if (similarDocuments.isNullOrEmpty()) return null
         val context = similarDocuments.filter { it.isText }.sortedBy { it.score }.mapNotNull { it.text }
+        val inputs = context.joinToString(separator = "\n") { it.replace("'", "''") }
         val sql = """
             SELECT pgml.transform(
                 task   => '{
                  "task": "summarization",
                  "model": "facebook/bart-large-cnn"
                }'::JSONB, 
-                inputs => ARRAY${context.joinToString(prefix = "['", postfix = "']", separator = "', '"){ it.replace("'", "''") }.trimIndent()},
-                args => '{"max_new_tokens": 25}'::JSONB
+                inputs => ARRAY['${inputs}'],
+                args => '{"min_new_tokens": 50}'::JSONB
             )
             """.trimIndent()
-        return jdbcTemplate.queryForObject(sql, String::class.java)
+        return jdbcTemplate.query(sql) { rs, _ ->
+            val summary = rs.getString(1)
+            objectMapper.readValue<List<Result>>(
+                summary,
+                objectMapper.typeFactory.constructCollectionType(
+                    List::class.java, Result::class.java
+                )
+            ).firstOrNull()
+        }.firstOrNull()
     }
 }
